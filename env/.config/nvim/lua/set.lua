a9f39e75fb6c9b47c916dd0312005593d649fc98
leonid-shutov <leonid.shutov.main@gmail.com>
2025-09-29 23:25:35 +0200

    langmap

--- env/.config/nvim/lua/set.lua
+++ env/.config/nvim/lua/set.lua
@@ -10,3 +10,6 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
 vim.opt.foldenable = true
+vim.opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯЖ;ABCDEFGHIJKLMNOPQRSTUVWXYZ:,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+vim.cmd([[nnoremap ; $]])
+
